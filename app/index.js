5903d4e4686105cae01e9fea994ae47259b8f0d8
chrisli4 <chrisli41@gmail.com>
2018-08-30 16:30:10 -0700

    initial commit

---
+++ app/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { TouchableHighlight, View, Text, StyleSheet } from 'react-native'
+
+import { connect } from 'react-redux'
+import { fetchData } from '../actions'
+
+let styles
+
+const App = (props) => {
+  const {
+    container,
+    text,
+    button,
+    buttonText
+  } = styles
+
+  return (
+    <View style={container}>
+      <Text style={text}>Redux 1212Examples</Text>
+    </View>
+  )
+}
+
+styles = StyleSheet.create({
+  container: {
+    marginTop: 100
+  },
+  text: {
+    textAlign: 'center'
+  },
+  button: {
+    height: 60,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0b7eff'
+  },
+  buttonText: {
+    color: 'white'
+  }
+})
+
+function mapStateToProps (state) {
+  return {
+    appData: state.appData
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    fetchData: () => dispatch(fetchData())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
